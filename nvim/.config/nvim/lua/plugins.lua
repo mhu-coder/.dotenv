a218be2a0d6ccd27c7e6536af74caff1b681f582
mhu-coder <mathieu.hu12@gmail.com>
2022-10-20 05:48:46 +0200

    [nvim]: Ensure packer is installed

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -1,5 +1,18 @@
 -- Plugin config. Requires packer to be available
 
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
 local res = require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
 
@@ -50,10 +63,11 @@ local res = require('packer').startup(function(use)
   -- Misc
   use 'tpope/vim-surround'
   use 'vimwiki/vimwiki'
+
+  if packer_bootstrap then
+    require('packer').sync()
+    print('Please restart nvim to load the plugins')
+  end
 end)
 
-vim.cmd("PackerInstall")
-
-require('lsp_config')
-
-return res
+return packer_bootstrap
