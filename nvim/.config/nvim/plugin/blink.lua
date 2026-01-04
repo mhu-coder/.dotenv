fc666442b0f7879607a4dd4203ed6ee8c30f4e70
mhu-coder <mathieu.hu12@gmail.com>
2026-01-04 17:36:49 +0100

    [nvim]: Enable doc in autocomplete

--- nvim/.config/nvim/plugin/blink.lua
+++ nvim/.config/nvim/plugin/blink.lua
@@ -6,6 +6,7 @@ end
 
 blink.setup({
   signature = { enabled = true },
+  completion = { documentation = { auto_show = true, auto_show_delay_ms = 500 } },
   snippets = { preset = 'luasnip' },
   sources = {
     default = { 'lsp', 'snippets', 'buffer' },
